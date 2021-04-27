0e310351d5fc1b700376df8add66236b5675e6ef
long chen <2833784463@qq.com>
2021-04-27 21:22:53 +0800

    页面配置

---
+++ lib/5120181550/Page_Help.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Page_Help extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('帮助'),
+        ),
+        body: Center(
+          child: Text('帮助页'),
+        )
+    );
+  }
+
+}
--- lib/5120181550/Page_Main.dart
+++ lib/5120181550/Page_Main.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:loginapp/5120181550/Page_Help.dart';
 
-class Page_Main extends StatelessWidget{
+class Page_Main extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _Page_MainState();
+  }
+
+}
+
+class _Page_MainState extends State<Page_Main>{
+
+  void _help(){
+    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=>Page_Help()));
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
         appBar: AppBar(
-          title: Text('Page_Main'),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('浏览器'),
+              IconButton(icon: Icon(Icons.help_outline), onPressed: _help)
+            ],
+          ),
         ),
         body: Center(
           child: Text('主页'),
