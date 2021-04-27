dd6bc7bb4a5753702dedf0625874416e112b54e0
long chen <2833784463@qq.com>
2021-04-27 19:45:28 +0800

    修改用户管理配置

--- lib/DataBase.dart
+++ lib/DataBase.dart
@@ -1,3 +1,36 @@
-class User{
+import 'dart:typed_data';
 
+class User{
+  String name;
+  String number;
+  String pass;
+  String nickname;
+  ByteBuffer icon;
+  int level=1;
+  int sex=0;
+  User(
+      this.name,
+      this.nickname,
+      this.number,
+      this.pass,
+      this.level,
+      this.icon,
+      this.sex
+      );
+
+  static List users=List();
+
+  static bool check(String numder,String pass){
+    for(User user in users){
+      if(user.number==numder&&user.pass==pass)
+        return true;
+    }
+    return false;
+  }
+
+  static void init(){
+    users.add(User('陈龙', '万年老二','19960209350', '123456', 1, null, 1));
+    users.add(User('邹存玉', '万年老二','19960209351', '123456', 1, null, 0));
+    users.add(User('杨璇', '万年老二','19960209352', '123456', 1, null, 0));
+  }
 }
